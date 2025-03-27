804679ad31f6851eea1568ef6c8a5aafea09971c
yuukishouda <shoudathird@outlook.jp>
2025-03-27 09:05:49 +0900

    fix

--- vercel-nestjs-test/src/main.ts
+++ vercel-nestjs-test/src/main.ts
@@ -4,12 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  // await app.listen(process.env.PORT ?? 3000);
-  await app.init();
-  return app;
+  await app.listen(process.env.PORT ?? 3000);
 }
-// bootstrap();
-export default async function handler(req: Request, res: Response, callback: Callback) {
-  const app = await bootstrap();
-  app.getHttpAdapter().getInstance()(req, res, callback);
-}
+bootstrap();
